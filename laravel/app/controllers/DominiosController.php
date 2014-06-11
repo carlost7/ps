0281a02898cc6a618d194444c89fe1bd62ebc929
carlos.juarez <t7marketing004@gmail.com>
2014-06-11 17:17:34 -0500

    cambiando la version del servidor

--- laravel/app/controllers/DominiosController.php
+++ laravel/app/controllers/DominiosController.php
@@ -154,6 +154,7 @@ class DominiosController extends BaseController {
                   {
                         //4.-
                         $plan_model = $this->Plan->mostrarPlan($plan_id);
+                        dd($plan_model);
                         if ($this->Dominio->apartarDominio($usuario, $dominio, $dominio_ajeno, $plan_model))
                         {
                               //5.-

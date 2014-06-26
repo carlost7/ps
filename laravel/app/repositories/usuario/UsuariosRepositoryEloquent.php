d7d6fa25e14208deb5c59e31b2b1ad6da580339e
carlos.juarez <t7marketing004@gmail.com>
2014-06-26 10:30:38 -0500

    pagination user and mail

--- laravel/app/repositories/usuario/UsuariosRepositoryEloquent.php
+++ laravel/app/repositories/usuario/UsuariosRepositoryEloquent.php
@@ -18,7 +18,7 @@ class UsuariosRepositoryEloquent implements UsuariosRepository {
 
       public function listarUsuarios()
       {
-            return User::where('is_admin', false)->pagination(10);
+            return User::where('is_admin', false)->paginate(10);
       }
 
       /*

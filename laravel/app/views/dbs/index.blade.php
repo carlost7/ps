29ecb616b6e347bce450cd24eb4171309a97a74e
carlos.juarez <t7marketing004@gmail.com>
2014-06-26 11:02:01 -0500

    modificacion acesso db

--- laravel/app/views/dbs/index.blade.php
+++ laravel/app/views/dbs/index.blade.php
@@ -42,7 +42,7 @@
 
                               <td>{{ HTML::link('dbs/'.$db->id,$db->nombre) }}</td>
                               <td>{{ $db->usuario }}</td>
-                              <td>{{ HTML::link('http://primerserver.com/db/','Webmail') }}</td>
+                              <td>{{ HTML::link('http://primerserver.com/db/','Accesar') }}</td>
                               <td>{{ $quotas[$db->nombre].'mb' }}</td>
                               <td>
                                     {{ Form::open(array('route' => array('dbs.destroy',$db->id),'method'=>'DELETE','id'=>$db->id)) }}

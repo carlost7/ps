88f26af76ff19ef5262ce836c9f1ba3b5983592b
carlos.juarez <t7marketing004@gmail.com>
2014-10-16 10:36:35 -0500

    database credentials

---
+++ laravel/app/config/enom.php
@@ -0,0 +1,8 @@
+<?php
+
+return array(  
+      'url' => 'https://resellertest.enom.com/interface.asp?',
+      'uid' => 'Wortev',
+      'pw' => 'Wortev124578',
+      'responsetype' => 'xml',
+);
---
+++ laravel/app/config/payment.php
@@ -0,0 +1,13 @@
+<?php
+
+/**
+ * Credenciales para MercadoPago
+ *
+ * @author carlos
+ */
+return array(
+      'client_id' => '3820658227334134',
+      'client_socket' => 'o04CCiTgVCCaMbjK87Wd3bK0dWqYt2GC',
+      'init_point' => 'sandbox_init_point',
+      'sandbox_mode' => true,
+);
